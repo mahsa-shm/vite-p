0e9415105640139baed45d10f6d93faffb8feb4f
mahsa-shahmohammadi <shmahsa464@gmail.com>
2024-11-25 09:41:43 +0330

    change: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './App.css'
 import App from './App.jsx'
-
+// import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
