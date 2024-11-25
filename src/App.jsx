304d3e0874ecc0a7311be3cbe8ecf58b51f269f1
mahsa-shahmohammadi <shmahsa464@gmail.com>
2024-11-25 09:26:34 +0330

    add: style and didMount

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import Home from './pages/Home.jsx'
 import './App.css'
-
+import "./index.css"
+import './Style.css'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
 function App() {
+
 
   return (
     <>
-      <div>hello git</div>
+      {/* <Practice2/> */}
+      <Home/>
     </>
   )
 }
